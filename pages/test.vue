3734bdbf5d3cb6bc789e0f38c903037046d42f75
flyingboy007 <abhilashvr007@gmail.com>
2021-07-24 07:30:18 +0530

    initial commit

---
+++ pages/test.vue
@@ -0,0 +1,27 @@
+<template>
+  <ejs-grid></ejs-grid>
+</template>
+
+<script>
+import Vue from 'vue'
+import { GridPlugin } from '@syncfusion/ej2-vue-grids'
+
+Vue.use(GridPlugin)
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style>
+@import '../node_modules/@syncfusion/ej2-base/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-buttons/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-calendars/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-dropdowns/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-inputs/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-navigations/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-popups/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-splitbuttons/styles/material.css';
+@import "../node_modules/@syncfusion/ej2-vue-grids/styles/material.css";
+</style>
